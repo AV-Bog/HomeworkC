2166849b01fc0c07230d5369ed07cc84b3fe7300
Bogdanova Arina <arinabogdanova1967@mail.ru>
2025-01-08 16:52:23 +0300

    Add files via upload

---
+++ brackets/stack.c
@@ -0,0 +1,49 @@
+﻿#include "stack.h"
+
+bool push(Stack** head, char element) {
+    Stack* temp = malloc(sizeof(Stack));
+    if (temp == NULL) {
+        return false;
+    }
+    temp->element = element;
+    temp->next = *head;
+    *head = temp;
+    return true;
+}
+
+void pop(Stack** head, int *errorCode) {
+    if (*head == NULL) {
+        *errorCode = 6;
+        return "0";
+    }
+
+    *errorCode = 0;
+    char value = (*head)->element;
+
+    Stack* temp = (*head)->next;
+    free(*head);
+    *head = temp;
+    return value;
+}
+
+bool isEmpty(Stack* head) {
+    return head == NULL;
+}
+
+char* peek(Stack* head, int* errorCode) {
+    if (isEmpty(head)) {
+        *errorCode = 6;
+        return NULL;
+    }
+
+    *errorCode = 0;
+    return head->element;
+}
+
+void cleanUpStack(Stack **head) {
+    while (!isEmpty(*head)) {
+        Stack* temp = *head;
+        *head = (*head)->next;
+        free(temp);        
+    }
+}
